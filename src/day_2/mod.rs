03b76f704b019ef38030aa01be0e6f25f7e82bc1
Yomi Ikuru <captyomjnr@gmail.com>
2023-12-02 23:16:11 +0000

    add: Template for Day 2 to Day 5

---
+++ src/day_2/mod.rs
@@ -0,0 +1,6 @@
+/**
+* Advent of code 2023, Day 2: Cube Conundrum
+* https://adventofcode.com/2023/day/2
+*/
+mod part_1;
+mod part_2;
---
+++ src/day_4/part_1.rs
@@ -0,0 +1,29 @@
+#[allow(dead_code)]
+fn part_1(input: &str) -> i32 {
+    let _placeholder = input;
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::util::print_green;
+
+    #[test]
+    fn test_part_1() {
+        let input = r#"
+"#
+        .trim_start_matches('\n');
+
+        let result = part_1(input);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn run_part_1() {
+        // let input = include_str!("./input.txt");
+        //
+        // let result = part_1(input);
+        print_green(&format!("Day X result: {}", 0));
+    }
+}
