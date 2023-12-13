6d97571fefdc06787639a203665e5d31423b34f7
Yomi Ikuru <captyomjnr@gmail.com>
2023-12-13 13:13:16 +0000

    fix: Update names of problems

--- src/day_10/mod.rs
+++ src/day_10/mod.rs
@@ -1,5 +1,5 @@
 /**
-* Advent of code 2023, Day 10: ---
+* Advent of code 2023, Day 10: Pipe Maze
 * https://adventofcode.com/2023/day/10
 */
 mod part_1;
--- src/day_11/mod.rs
+++ src/day_11/mod.rs
@@ -1,5 +1,5 @@
 /**
-* Advent of code 2023, Day 11: ---
+* Advent of code 2023, Day 11: Cosmic Expansion
 * https://adventofcode.com/2023/day/11
 */
 mod part_1;
