c49c715754e6aae879477dfb51c681bc6cd3b7c7
Yomi Ikuru <captyomjnr@gmail.com>
2023-12-13 02:18:36 +0000

    add: Point utils - Seems to be used often

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,37 @@
+use std::ops::{Add, Sub};
+
 #[cfg(test)]
 pub fn print_green(message: &str) {
     println!("\x1b[32m{}\x1b[0m", message);
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct Point {
+    pub x: i64,
+    pub y: i64,
+}
+impl Point {
+    pub fn new(x: i64, y: i64) -> Self {
+        Self { x, y }
+    }
+}
+impl Sub for Point {
+    type Output = Point;
+
+    fn sub(self, other: Point) -> Point {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
 }
